aef04430fe099af0433bfc99547fea870fcab90a
tony_kero <antoine.karcher.id@gmail.com>
2017-03-22 14:23:01 +0100

    Added Differential Evolution tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,7 +3,7 @@
 
 #include <moe/moe.hpp>
 
-TEST_CASE( "Genetic Algorithm tests", "[genetic]" )
+TEST_CASE( "Genetic Algorithm tests", "[genetic_algorithm]" )
 {
     auto dataset = moe::util::getAlphabet<char>();
     dataset.push_back(' ');
@@ -65,5 +65,28 @@ TEST_CASE( "Genetic Algorithm tests", "[genetic]" )
         CAPTURE( genotype );
         CAPTURE( ga.getBestMoe().fitness );
         REQUIRE( ga.getBestMoe().fitness <= 4 );
+    }
+}
+
+TEST_CASE( "Differential Evolution Tests", "[differential_evolution]" )
+{
+    DifferentialEvolution<int> de(20);
+
+    long long n = 2261953600;
+    
+    de.setFitnessFunction( [n](auto moe) -> double
+    {
+        long long genotype = moe.genotype[0];
+        double error = std::abs(n - genotype*genotype);
+        return 1/(error+1);
+    });
+
+    SECTION( "Should return an optimal solution" )
+    {
+        de.run(50);
+
+        CAPTURE( de.getBestMoe().genotype[0] );
+        CAPTURE( de.getBestMoe().fitness );
+        REQUIRE( de.getBestMoe().fitness == Approx(1).margin(1));
     }
 }
