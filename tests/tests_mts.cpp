3534fa6cdb3bf21b8365df03c105183904ba9147
tony_kero <antoine.karcher.id@gmail.com>
2017-04-05 13:31:07 +0200

    Added Mutations tests

---
+++ tests/tests_mts.cpp
@@ -0,0 +1,70 @@
+TEST_CASE( "Mutations Tests", "[mutations]" )
+{
+    std::default_random_engine engine;
+    auto dataset = moe::util::getAlphabet<char>();
+    
+    Substitution<char>  s(engine);
+    Insertion<char>     i(engine);
+    Deletion<char>      d(engine);
+    Translocation<char> t(engine);
+
+    const std::vector<char> sample = { 'a', 'b', 'c', 'd' };
+
+    SECTION( "Substitution Mutation" )
+    {
+
+        auto post_sample = s.mutate(sample, dataset);
+
+        unsigned int    pre_size    = sample.size(),
+                        post_size   = post_sample.size();
+        unsigned int    count = 0;
+        
+        for(unsigned int index = 0; index < pre_size-1; index++)
+        {
+            if( sample[index] != post_sample[index] )
+                count++;
+        }
+
+        CAPTURE( sample );
+        CAPTURE( post_sample );
+        REQUIRE( pre_size == post_size );
+        REQUIRE( count == 1 );
+    }
+
+    SECTION( "Insertion Mutation" )
+    {
+        unsigned int    pre_size    = sample.size(),
+                        post_size   = i.mutate(sample, dataset).size();
+    
+        REQUIRE( pre_size+1 == post_size );
+    }
+
+    SECTION( "Deletion Mutation" )
+    {
+        unsigned int    pre_size    = sample.size(),
+                        post_size   = d.mutate(sample, dataset).size();
+    
+        REQUIRE( pre_size-1 == post_size );
+    }
+
+    SECTION( "Translocation Mutation" )
+    {
+        auto post_sample = t.mutate(sample, dataset);
+
+        unsigned int    pre_size    = sample.size(),
+                        post_size   = post_sample.size();
+        unsigned int    count = 0;
+        
+        for(unsigned int index = 0; index < pre_size; index++)
+        {
+            if( sample[index] != post_sample[index] )
+                count++;
+        }
+
+        CAPTURE( sample );
+        CAPTURE( post_sample );
+        REQUIRE( pre_size == post_size );
+        REQUIRE( count == 2 );
+    }
+
+}
